646c43d8998a0336abd3d114e587597f52a66581
marvinlab <m.labrador@klab.com>
2014-10-02 11:28:55 +0800

    Chapter 13 TouchTracker (used UIGestureRecognizer: doubleTap clears the drawing area)

--- TouchTracker/TouchTracker/BNRDrawView.m
+++ TouchTracker/TouchTracker/BNRDrawView.m
@@ -32,6 +32,16 @@
         self.finishedLines = [[NSMutableArray alloc]init];
         self.backgroundColor = [UIColor grayColor];
         self.multipleTouchEnabled = YES;
+        
+        UITapGestureRecognizer *doubleTapRecognizer =
+                    [[UITapGestureRecognizer alloc]initWithTarget:self
+                                                          action:@selector(doubleTap:)];
+        doubleTapRecognizer.numberOfTapsRequired = 2;
+        doubleTapRecognizer.delaysTouchesBegan = YES;
+        
+        [self addGestureRecognizer:doubleTapRecognizer];
+        
+        
     }
     
     return self;
@@ -165,6 +175,16 @@
         [self.linesInProgress removeObjectForKey:key];
     }
     
+    [self setNeedsDisplay];
+    
+}
+
+
+- (void)doubleTap: (UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized DoubleTap");
+    [self.linesInProgress removeAllObjects];
+    [self.finishedLines removeAllObjects];
     [self setNeedsDisplay];
     
 }
